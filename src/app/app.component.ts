a72b12d2d192ee0fdf4821f14bb2eb9a2a241175
alihasnain-cd <ali.hasnain@codedistrict.com>
2021-07-13 16:22:37 +0500

    added sub app route for mf-screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'mf-header',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mf-header';
+
+  message: any = 'New message';
+
+  ngOnInit() {
+
+    localStorage.setItem('dataSource', 'From Header');
+
+
+  }
+
+
+  deliverMessage(target) {
+
+    console.log("delivering message", this.message)
+    target.dispatchEvent(new CustomEvent('customEvent', {
+      detail: {
+        message: this.message,
+        data: { name: 'header' }
+      },
+      bubbles: true
+    }));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
@@ -12,7 +12,8 @@ import { EmptyRouteComponent } from './empty-route/empty-route.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
